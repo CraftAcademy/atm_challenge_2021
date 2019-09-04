f44c23955ebfd1fdbf968007912e5a4ad5877c45
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-09-04 12:43:20 +0200

    Add method to deactivate account along with a test
    Add method 'deactivate' to account.rb.
    Add test for deactivating account to account_spec.
    Choose instance method because it needs to act on a particular
    instance of the class.

--- lib/account.rb
+++ lib/account.rb
@@ -3,7 +3,7 @@ require 'date'
 class Account
   STANDARD_VALIDITY_YRS = 5
 
-  attr_accessor :pin_code, :exp_date
+  attr_accessor :pin_code, :exp_date, :account_status
   
   def set_expire_date
     Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
@@ -12,6 +12,12 @@ class Account
   def initialize
     @pin_code = rand(1000..9999)
     @exp_date = set_expire_date
+    @account_status = :active
   end
+
+  def deactivate
+    @account_status = :deactivated
+  end
+  
 
 end
