f5ef8ae81a16c6d0f39a8ab1277e73b9b93ca808
piavw <piavonwachenfelt@hotmail.com>
2019-09-04 13:28:26 +0200

    Creates lib/person, creates Person class, creates name methods.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+    attr_accessor :name, :cash, :account
+
+    def initialize(attrs= {}) 
+        set_name(attrs[:name]) 
+        @cash = 100
+        @account = :active
+    end
+
+    private
+    def set_name(obj)
+        obj == nil ? no_name : @name = obj 
+    end
+
+    def no_name
+        raise "A name is required" 
+    end
+
+end
