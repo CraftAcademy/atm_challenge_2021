b03648e696dadfbe706f98fbf0eec66bb335fa8c
luca <l.lobacher@web.de>
2019-09-04 13:09:59 +0200

    Adds instance_double of person

--- lib/account.rb
+++ lib/account.rb
@@ -9,14 +9,24 @@ class Account
     Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
   end
 
-  def initialize
+  def initialize(attrs = {})
     @pin_code = rand(1000..9999)
     @exp_date = set_expire_date
     @account_status = :active
+    set_owner(attrs[:owner])
   end
 
   def deactivate
     @account_status = :deactivated
+  end
+
+  private
+  def set_owner(obj)
+    obj == nil ?  missing_owner : @owner = obj
+  end
+
+  def missing_owner
+    raise "An Account owner is required"
   end
   
 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,6 +1,16 @@
 require './lib/account.rb'
 
 describe Account do
+let(:person) {instance_double('Person', name: 'Hans')}
+subject { described_class.new({owner: person}) }
+
+  it 'is expected to have an owner' do
+    expect(subject.owner).to eq person
+  end
+
+  it 'is expected to raise error if no owner is set' do
+    expect { described_class.new }.to raise_error 'An Account owner is required'
+  end
 
   it 'check the length of a number' do
     number = 1234
