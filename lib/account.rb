fd465a8c533af9a713d7776d5a54266adb631f56
luca <l.lobacher@web.de>
2019-09-04 13:25:38 +0200

    Adds :owner to attr_accessor

--- lib/account.rb
+++ lib/account.rb
@@ -3,7 +3,7 @@ require 'date'
 class Account
   STANDARD_VALIDITY_YRS = 5
 
-  attr_accessor :pin_code, :exp_date, :account_status
+  attr_accessor :pin_code, :exp_date, :account_status, :owner
   
   def set_expire_date
     Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
