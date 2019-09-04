ff607f3841c685d1938714ed9dbebb6d69bd51cc
luca <l.lobacher@web.de>
2019-09-04 11:36:12 +0200

    Fixes no method error in account class

--- lib/account.rb
+++ lib/account.rb
@@ -6,15 +6,12 @@ class Account
   attr_accessor :pin_code, :exp_date
   
   def set_expire_date
-    Date.today.next_year(Account: STANDARD_VALIDITY_YRS).strftime('%m/%y')
+    Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
   end
 
   def initialize
     @pin_code = rand(1000..9999)
-    #@exp_date = Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
     @exp_date = set_expire_date
   end
-
- 
 
 end
