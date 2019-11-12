c2dba2eabfd85c86aa77e0ee22b885a628a529f2
Johan <sundqvist.johan@outlook.com>
2019-11-12 11:11:47 +0100

    Create instance_double Account with initial balance

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -8,5 +8,14 @@ describe ATM do
         subject.withdraw 50
         expect(subject.funds).to eq 950
     end
-    
+
+    let(:account) { instance_double('Account') }
+    before do
+        # Before each test we need to add an attribute to `balance`
+        # to the `account` object set set the value to `100`
+        allow(account).to receive(:balance).and_return(100)
+        # We also need to allow `account` to receive the new balance
+        # using the setter method `balance`
+        allow(account).to receive(:balance=)    
+    end
 end
