8613b4209e73801aaae2110539b6e84822412e0d
Eevan <eevan.redon@gmail.com>
2019-11-12 14:26:13 +0100

    adding spec test for pin code and implement new when in withdraw method when pin is incorrect

--- lib/atm.rb
+++ lib/atm.rb
@@ -5,12 +5,14 @@ class ATM
         @funds = 1000
     end
 
-    def withdraw(amount, account)
+    def withdraw(amount, pin_code, account)
         case
         when insufficient_funds_in_account?(amount, account)
             {status: false, message: 'insufficient funds', date: Date.today}
         when insufficient_funds_in_atm?(amount)
             {status: false, message: 'insufficient funds in ATM', date: Date.today}
+        when incorrect_pin?(pin_code, account.pin_code)
+            {status: false, message: 'wrong pin', date: Date.today}
         else
             perform_transaction(amount, account)
         end
@@ -30,6 +32,10 @@ class ATM
 
     def insufficient_funds_in_atm?(amount)
         @funds < amount
+    end
+
+    def incorrect_pin?(pin_code, actual_pin)
+        pin_code != actual_pin
     end
 
 end
