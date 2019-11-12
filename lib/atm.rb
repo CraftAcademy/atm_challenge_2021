43b814e877924f5843642a4a741989e07d730e04
Eevan <eevan.redon@gmail.com>
2019-11-12 11:47:11 +0100

    adds withdraw spec test to out put 'reciept' hash  and modify withdraw function to update account balance, errors are thrown

--- lib/atm.rb
+++ lib/atm.rb
@@ -5,8 +5,15 @@ class ATM
         @funds = 1000
     end
 
-    def withdraw(amount)
-        @funds -= amount
+    def withdraw(amount, account)
+        case
+        when amount > account.balance
+            return
+        else
+            @funds -= amount
+            account.balance = account.balance - amount
+            {status: true, message: 'success', date: Date.today, amount: amount}
+        end
     end
 
 end
