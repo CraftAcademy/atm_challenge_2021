c36aebac91e0cc41466e17aabb7c8b0951fb08ed
Johan <sundqvist.johan@outlook.com>
2019-11-13 11:37:09 +0100

    Adds account status on init, adds deactivate account method

--- lib/account.rb
+++ lib/account.rb
@@ -1,16 +1,18 @@
 class Account
 
     STANDARD_VALIDITY_YRS = 5
-    attr_accessor :exp_date
+    attr_accessor :exp_date, :account_status
 
     def initialize
         @exp_date = set_expire_date
+        @account_status = :active
     end
 
     def set_expire_date
         Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
     end
 
-
-
+    def deactivate
+        @account_status = :deactivated
+    end
 end
