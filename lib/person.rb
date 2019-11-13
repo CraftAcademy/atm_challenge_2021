b87f009d94f1cacf31c380664d03c42b49bf833f
Eevan <eevan.redon@gmail.com>
2019-11-13 16:24:56 +0100

    adding person class and appropriate tests

---
+++ lib/person.rb
@@ -0,0 +1,29 @@
+class Person
+    attr_accessor :name, :cash, :account
+
+    def initialize(attrs = {})
+        @name = set_name(attrs[:name])
+        @cash = 0
+        @account = nil
+    end
+
+    def set_name(name)
+        name == nil ? missing_name : name
+    end
+
+    def missing_name
+        raise "A name is required"
+    end
+
+    def create_account
+        @account = Account.new(owner: self) 
+    end
+
+    def deposit()
+    end
+
+
+
+
+
+end
