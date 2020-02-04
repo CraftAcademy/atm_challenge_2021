1153e199839f49918d1f4444cc649b430d66f084
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-02-04 16:37:07 +0100

    added test to see if expiration date is set on initialization, and method to set expiration date

--- lib/account.rb
+++ lib/account.rb
@@ -3,11 +3,15 @@
 class Account
     attr_accessor(:pin_code, :balance, :account_status, :exp_date)
 
-def initialize 
-    @pin_code = rand(1000..9999)
-end
+    STANDARD_VALIDITY_YRS = 5
+
+    def initialize 
+        @pin_code = rand(1000..9999)
+        @exp_date = set_expire_date()
+    end 
+
+    def set_expire_date
+        Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
+    end
 
 end
-
-
-
