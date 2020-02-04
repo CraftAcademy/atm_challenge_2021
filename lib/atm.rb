92138e9586ca5f5372a6f820227c516849ce1bb4
pauloswardblad <paulo@swardblads.se>
2020-02-04 10:06:13 +0100

    adding case in lib/atm.rb

--- lib/atm.rb
+++ lib/atm.rb
@@ -8,4 +8,15 @@ class Atm
     def withdraw(amount, account)
         @funds -= amount
     end
+
+    case
+    when amount > account.balance   
+    else
+        @funds -= amount
+        account.balance = account.balance - amount
+        { status: true, message: 'success', date: '2020', amount: amount}
+    end
+
+
+
 end
