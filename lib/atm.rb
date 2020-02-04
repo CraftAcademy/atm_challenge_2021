30f98f0b56398f44d42b0c9f68599b519c443979
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-02-04 10:10:19 +0100

    added withdraw method, tests and reciept function

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,11 +1,27 @@
 class Atm
     attr_accessor :funds
-    
+
+
     def initialize 
         @funds = 1000
     end
 
     def withdraw(amount)
-        @funds -= amount
+        result = {}
+    
+        if @funds >= 0
+            @funds -= amount
+            result = {  status: true, message: "Successful", date: Time.now.strftime("%Y-%m-%d"), 
+                        amount: amount}
+        else
+            result = {  status: false, message: "Fail because of {enter message here}", 
+                        date: Time.now.strftime("%Y-%M-%d"), amount: amount, remaining: @funds }
+        end 
+        return result
     end
+end
+
+
+class printReciept(errorCode)
+    @errorCode = errorCode
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,7 +1,14 @@
 require './lib/atm.rb'
 
 describe Atm do
-    
+    let(:account) { instance_double('Account') }
+
+    before do
+      allow(account).to receive(:balance).and_return(100)
+      allow(account).to receive(:balance=)
+    end
+
+
     it 'has 1000$ on initialize' do
         expect(subject.funds).to eq 1000
     end
@@ -9,6 +16,13 @@ describe Atm do
     it 'funds are reduced at withdraw' do 
         subject.withdraw 50
         expect(subject.funds).to eq 950
+    end
+
+    it 'respons message if the withdrawl was successful' do
+        subject.withdraw 900
+        expect(subject.funds).to be >= 0
+        expected_output = { status: true, message: 'Successful', date: Time.now.strftime("%Y-%m-%d"), amount: 900 }
+        expect(subject.withdraw(900)).to eq expected_output
     end
 
 end
