249b57e7fcbd9de93ae94a5b1fa5d0fe445c9f03
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-02-04 14:08:15 +0100

    added expired card tests and functions to see that the user uses a valid card

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Atm
     attr_accessor :funds
 
@@ -15,6 +17,8 @@ class Atm
             { status: false, message: 'Unsuccessful because insufficient funds in ATM', date: Time.now.strftime("%Y-%m-%d") }
         when incorrect_pin?(pin_code, account.pin_code)
             { status: false, message: 'Unsuccessful because of wrong pin code', date: Time.now.strftime("%Y-%m-%d") }
+        when card_expired?(account.exp_date)
+            { status: false, message: 'Unsuccessful because card has expired', date: Time.now.strftime("%Y-%m-%d") }
         else
             perform_transaction(amount, account)
         end
@@ -39,4 +43,9 @@ class Atm
     def incorrect_pin?(pin_code, actual_pin)
         pin_code != actual_pin
     end 
+
+    def card_expired?(exp_date)
+        Date.strptime(exp_date, '%m/%y') < Date.today
+    end
+    
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,7 +1,7 @@
 require './lib/atm.rb'
 
 describe Atm do
-    let(:account) { instance_double('Account', pin_code: '1234') }
+    let(:account) { instance_double('Account', pin_code: '1234', exp_date: '04/21') }
 
     before do
       allow(account).to receive(:balance).and_return(100)
@@ -38,4 +38,11 @@ describe Atm do
         expected_output = { status: false, message: 'Unsuccessful because of wrong pin code', date: Time.now.strftime("%Y-%m-%d") }
         expect(subject.withdraw(100, '1235', account)).to eq expected_output
     end 
+
+    it 'reject the withdraw if the card is expired' do 
+        allow(account).to receive(:exp_date).and_return('12/15')
+        expected_output = { status: false, message: 'Unsuccessful because card has expired', date: Time.now.strftime("%Y-%m-%d") }
+        expect(subject.withdraw(20, '1234', account)).to eq expected_output
+    end
+
 end
