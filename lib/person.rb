81d1ac35c9ae4568767cbf2448c8fb4d434ff408
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-05 12:16:28 +0100

    adding a test to make sure that the cash attribute is set to 0 and then passing that into the funtionality to pass it. Also writing a test to see that the account attribute exist and passing that. Currently stuck on the test for creating an account and the funtionality to pass it

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,21 @@
 class Person
-    attr_accessor(:name)
+    attr_accessor(:name, :cash, :account)
 
     def initialize(attrs = {})
         set_name(attrs[:name])
+        @cash = 0
+        @account 
     end
     
     def set_name(obj)
         obj == nil ? missing_name : @name = obj
+    end
+
+    def missing_name
+       raise 'A name is required'
+    end
+
+    def create_account
+        subject = Account.new
     end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -8,4 +8,27 @@ describe Person do
     it 'is expected to have a :name on initialize' do
         expect(subject.name).not_to be nil
     end 
+
+    it 'is expected to raise an error if no name is set' do 
+        expect { described_class.new }.to raise_error 'A name is required'
+    end
+
+    it 'is expected to have a :cash attribute with the value of 0 on initialize' do
+        expect(subject.cash).to eq 0
+    end
+
+    it 'is expected to have a :account attribute' do 
+        expect(subject.account).to be nil
+    end
+
+end 
+ 
+describe 'can create an Account' do 
+    
+    before { subject.create_account }
+
+    it 'of Account class' do
+        expect(subject.account).to be_an_instance_of Account
+    end
+
 end
