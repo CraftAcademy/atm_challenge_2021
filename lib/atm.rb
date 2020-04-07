808dcc2fc9989171d3c7ef6b221747ad8446bd1d
designerofthing <designerofthings@gmail.com>
2020-04-07 17:19:17 +0200

    adding test and method for checking card expiration date

--- lib/atm.rb
+++ lib/atm.rb
@@ -5,7 +5,7 @@ class Atm
         @funds = 1000
     end
 
-    def withdraw(amount, pin_code, account)
+    def withdraw(amount, pin_code, account, exp_date)
         case
         when insufficient_funds_in_account?(amount, account)
             { status: false, message: 'insufficient funds in account', date: Date.today}
@@ -13,6 +13,8 @@ class Atm
             { status: false, message: 'insufficient funds in ATM', date: Date.today}    
         when incorrect_pin?(pin_code, account.pin_code)
             { status: false, message: 'wrong pin', date: Date.today}
+        when card_expired?(account.exp_date)
+            { status: false, message: 'card expired', date: Date.today}
         else
             perform_transaction(amount, account)
         end            
@@ -35,6 +37,9 @@ class Atm
     end
     def incorrect_pin?(pin_code, actual_pin)
         pin_code != actual_pin
-        end
-    end 
+    end
+     def card_expired?(exp_date)
+         Date.strptime(exp_date, '%m/%y') < Date.today
+     end
+end 
 
