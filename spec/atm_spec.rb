3fee5e649d6bb0344b6429851ef45c07f7ec2c25
designerofthing <designerofthings@gmail.com>
2020-04-08 16:17:45 +0200

    adding bills algorithm and output

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -15,7 +15,12 @@ describe Atm do
       expect(subject.funds).to eq 950
     end
     it 'allows withdrawal if the account has enough balance.' do
-      expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+      expected_output = { 
+        status: true,
+        message: 'success',
+        date: Date.today, 
+        amount: 45,
+        bills: [20, 20, 5]}
       expect(subject.withdraw(45, '1234', account, :exp_date, :account_status)).to eq expected_output
     end
 
@@ -42,5 +47,6 @@ describe Atm do
       expected_output = { status: false, message: 'account is disabled', date: Date.today }
       expect(subject.withdraw(6, '1234', account, :exp_date, :account_status)).to eq expected_output
     end
+    
 end
 
