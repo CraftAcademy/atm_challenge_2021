a45e6a283cf1cc02ea7afd55d63079dba7ef7311
Erik Björn <erik.g.bjoern@gmail.com>
2020-04-08 21:46:42 +0200

    Creating new instance of account with owner, creating tests for having owner and missing owner:

--- lib/account.rb
+++ lib/account.rb
@@ -1,15 +1,29 @@
+require 'date'
+
 class Account
-    attr_accessor :pin_code, :exp_date, :account_status
+    attr_accessor :pin_code, :exp_date, :account_status, :owner
     STANDARD_VALIDITY_YRS = 5
 
 
-    def initialize
+
+    def initialize(attrs = {})
         @pin_code = rand(1000..9999)
-        @exp_date = Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
         @account_status = :active
+        @exp_date = Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
+        set_owner(attrs[:owner])
     end
 
     def deactivate
         @account_status = :deactivated
     end
+
+    private
+    def set_owner(obj)
+        obj == nil ? missing_owner : @owner = obj
+    end
+
+    def missing_owner
+        raise "An Account owner is required"
+    end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,6 +1,10 @@
 require './lib/account.rb'
 require 'date'
+
 describe Account do
+    let(:person) { instance_double('Person', name: 'Thomas') }
+    subject { described_class.new({owner: person}) }
+
     it 'has a four digit PIN code' do
         expect(subject.pin_code.to_s.length).to eq 4
     end
@@ -15,5 +19,10 @@ describe Account do
         subject.deactivate
         expect(subject.account_status).to eq :deactivated
     end
-    
+    it 'is expected to have an owner' do
+        expect(subject.owner).to eq person
+    end
+    it 'is expected to raise error if no owner is set' do
+        expect { described_class.new }.to raise_error 'An Account owner is required'
+    end
 end
