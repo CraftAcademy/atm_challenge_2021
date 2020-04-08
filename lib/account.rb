db9295c706bf2ef0e3b1858cde003927fa28a54b
theodora <theo87.a@gmail.com>
2020-04-08 17:34:01 +0200

    adding exp_date

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,10 @@
 class Account
-    attr_accessor :pin_code
+    attr_accessor :pin_code, :exp_date
+    STANDARD_VALIDITY_YRS = 5
+
 
     def initialize
         @pin_code = rand(1000..9999)
+        @exp_date = Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
     end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,7 +1,12 @@
 require './lib/account.rb'
-
+require 'date'
 describe Account do
     it 'has a four digit PIN code' do
         expect(subject.pin_code.to_s.length).to eq 4
     end
+    it 'is expected to have an expiry date on initialize' do
+        expected_date = Date.today.next_year(5).strftime("%m/%y")
+        expect(subject.exp_date).to eq expected_date
+    end
+    
 end
