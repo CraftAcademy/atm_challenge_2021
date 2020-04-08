ad8eb06b977e955185bbb040b5fb205686feec3c
designerofthing <designerofthings@gmail.com>
2020-04-08 17:49:46 +0200

    adding account_status test and implementation

--- lib/account.rb
+++ lib/account.rb
@@ -1,10 +1,15 @@
 class Account
-    attr_accessor :pin_code, :exp_date
+    attr_accessor :pin_code, :exp_date, :account_status
     STANDARD_VALIDITY_YRS = 5
 
 
     def initialize
         @pin_code = rand(1000..9999)
         @exp_date = Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
+        @account_status = :active
+    end
+
+    def deactivate
+        @account_status = :deactivated
     end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,5 +8,12 @@ describe Account do
         expected_date = Date.today.next_year(5).strftime("%m/%y")
         expect(subject.exp_date).to eq expected_date
     end
+    it 'is expected to have :active status on initialize' do
+        expect(subject.account_status).to eq :active
+    end
+    it 'deactivates account using the instance method' do  #otherwise all accounts would be deactivated
+        subject.deactivate
+        expect(subject.account_status).to eq :deactivated
+    end
     
 end
