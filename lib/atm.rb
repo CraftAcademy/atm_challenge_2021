b831698f900a458902e5055c39420635cc3289b4
Alex Bellamy <alexlbellamy@gmail.com>
2020-06-09 16:03:11 +0200

    adding tests and functionality to ensure sufficient funds inATM to dispense

--- lib/atm.rb
+++ lib/atm.rb
@@ -9,6 +9,8 @@ class Atm
         case 
             when insufficient_funds_in_account?(amount,account)
             { status: false, message: 'insufficient funds', date: Date.today}
+            when insufficient_funds_in_ATM?(amount)
+                {status: false, message: 'insufficient funds in ATM', date: Date.today }
             else
             perform_transaction(amount,account)
     end
@@ -16,6 +18,10 @@ class Atm
     
     
     private
+    def insufficient_funds_in_ATM?(amount)
+    @funds < amount
+    end
+    
     def insufficient_funds_in_account?(amount,account)
     amount > account.balance
     end
