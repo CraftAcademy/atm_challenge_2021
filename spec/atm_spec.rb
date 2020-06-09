1ecff05347a037154517ed4c89e980569b4e9f89
viktor_sommar <vsommarbaillot@gmail.com>
2020-06-09 15:23:51 +0200

    Adding implementation code to funds being reduced at withdrawal (previously removed). Tests passing.

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -14,4 +14,13 @@ describe Atm do
         expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
         expect(subject.withdraw(45, account)).to eq expected_output
     end
+
+    it 'has 1000$ on initialize' do
+        expect(subject.funds).to eq 1000
+    end
+
+    it 'funds are reducted at withdraw' do
+    subject.withdraw(50, account)
+    expect(subject.funds).to eq 950
+    end
 end
