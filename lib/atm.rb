d6e5c93a7880b352e06f5e968c5ccf17a10523bb
Alex Bellamy <alexlbellamy@gmail.com>
2020-06-10 15:26:57 +0200

    adding until loop in relation to withdrawl of cash - bills

--- lib/atm.rb
+++ lib/atm.rb
@@ -18,7 +18,7 @@ class Atm
             when account_disabled?(account_status)
                 {status: false, message: 'account is disabled', date: Date.today}
             else
-            perform_transaction(amount,account)
+            perform_transaction(amount,account,add_bills)
     end
         end
     
@@ -43,12 +43,24 @@ class Atm
     def perform_transaction(amount,account)
     @funds -= amount
     account.balance = account.balance - amount
-    {status: true, message: "success", date: Date.today, amount:amount}
+    {status: true, message: "success", date: Date.today, amount:amount, bills: add_bills(amount) }
 
+    def add_bills(amount)
+        denominations = [20, 10, 5]
+        bills = []
+        denominations.each do |bill|
+            while amount - bill >= 0
+                amount -= bill
+                bills << bill
+            end
+        end 
+        bills
+    end
     
     end
 
     def card_expired?(exp_date)
         Date.strptime(exp_date, '%m/%y') < Date.today
     end
+
 end
