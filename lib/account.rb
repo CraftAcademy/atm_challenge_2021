719bbb8a8d84ff0ad4bf844dabd2d503693e0ae5
odeane_croney <odeane_croney@yahoo.com>
2020-06-11 13:22:51 +0200

    refactoring expire date method to mirror course example

--- lib/account.rb
+++ lib/account.rb
@@ -5,10 +5,15 @@ STANDARD_VALIDITY_YRS = 5
 
     def initialize
         @pin_code = rand(1000..9999)
-
-       @exp_date = Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
+        @exp_date = set_expire_date
+       
        @account_status = :active
     end
+
+    def set_expire_date
+        Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
+    end
+
 
     def deactivate
         @account_status = :deactivated
