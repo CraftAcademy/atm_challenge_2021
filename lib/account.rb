5d3a8496f4c14a59c3c188ad5df1227138d09b00
viktor_sommar <vsommarbaillot@gmail.com>
2020-06-11 09:51:48 +0200

    adding test to check pin code lenght being equal to 4 digits. Tests passing.

--- lib/account.rb
+++ lib/account.rb
@@ -1,10 +1,7 @@
 class Account
     attr_accessor :pin_code, :balance, :account_status, :exp_date
 
-
-    def initialize 
+    def initialize
         @pin_code = rand(1000..9999)
-        
     end
-
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,14 +1,10 @@
 require './lib/account.rb'
 
 describe Account do
-   
-    
-    
-    it 'check the length of a number' do
-        @pin_code = rand(1000..9999)
-        number_length = Math.log10(pin_code).to_i + 1
-        expect(number_length).to eq 4
+
+    it 'check if pin has 4 digits' do
+        pin_code_length = Math.log10(subject.pin_code).to_i + 1
+        expect(pin_code_length).to eq 4
 
     end
-
 end
