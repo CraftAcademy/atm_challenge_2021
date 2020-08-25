87f1533f49fa80f2ca773e3d3d5c7c9d3bdfbcc6
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-08-25 11:15:44 +0200

    adding a new block with the expected_output

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,12 +1,31 @@
 require './lib/atm.rb'
+
 describe Atm do
+    let(account) { instance_double('Account') }
+
+    before do
+        allow(account).to recevie(:balance=).and_return(100)
+
+        allow(account).to recevie(:balance=)       
+    end
+
+
     it 'has 1000$ on initialize' do
         expect(subject.funds).to eq 1000
     end
     
+
     it 'funds are reduced at withdraw' do
         subject.withdraw 50
         expect(subject.funds).to eq 950
+    end
+    
+
+    it 'allow withdraw if the account has enough balance.' do
+
+        expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+
+        expect(subjcet.withdraw(45, account)).to eq expected_output
     end
 
 end
