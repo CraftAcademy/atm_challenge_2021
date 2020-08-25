2ea8c70caa9f8874c345380f2723e72e947dd4e3
Mauro  Avellaneda <mauroeavellaneda@gmail.com>
2020-08-25 16:58:31 +0200

    modifying the implementation code and passing the test

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,10 +1,12 @@
 require './lib/atm.rb'
 
 describe Atm do
+
     let(:account) { instance_double('Account') }
 
     before do
-        allow(account).to receive(:balance=).and_return(100)
+
+        allow(account).to receive(:balance).and_return(100)
 
         allow(account).to receive(:balance=)       
     end
@@ -24,8 +26,8 @@ describe Atm do
     it 'allow withdraw if the account has enough balance.' do
 
         expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
-
         expect(subject.withdraw(45, account)).to eq expected_output
+
     end
 
 end
