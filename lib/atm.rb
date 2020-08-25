6f0e951fd4a42c109d1530ef87f7cdd3d13aa5a5
Mauro  Avellaneda <mauroeavellaneda@gmail.com>
2020-08-25 17:55:29 +0200

    finishing the block where ATM rejects the withdrawal because of amount is greater than the account

--- lib/atm.rb
+++ lib/atm.rb
@@ -8,11 +8,13 @@ class Atm
     def withdraw(amount, account)
         case
         when insufficient_funds_in_account?(amount, account)
-            { satus: false, message: 'insufficient funds', date: Date.today }
+            { status: false, message: 'insufficient funds in account', date: Date.today }
+        when insufficient_funds_in_ATM?(amount)
+            { status: false, message: 'insufficient funds in ATM', date: Date.today }
       
         else
 
-    perform_transaction(amount, account)
+             perform_transaction(amount, account)
         end
     end
 
@@ -20,6 +22,9 @@ class Atm
 
     def insufficient_funds_in_account?(amount, account)
             amount > account.balance
+    end
+    def insufficient_funds_in_ATM(amount)
+        @funds < amount
     end
 
     def perform_transaction(amount, account)
