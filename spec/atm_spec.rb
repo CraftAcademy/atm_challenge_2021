f91e0d0b27f390e3e382e5db824ec17eb9589545
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-08-25 17:13:26 +0200

    Created method to return feedback to the user

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -27,4 +27,10 @@ describe Atm do
         expect(subject.withdraw(45, account)).to eq expected_output
     end
 
+    it 'rejects withdraw if account has insufficient funds' do
+        expected_output = { satus: false, message: 'insufficient funds', date: Date.today }
+        expect(subject.withdraw(105, account)).to eq expected_output
+
+    end
+
 end
