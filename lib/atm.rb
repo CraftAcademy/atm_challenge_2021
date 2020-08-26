93c4760f66b6187906243ca8b26212918f8c1b61
sara <saralundkvist86@gmail.com>
2020-08-26 09:24:30 +0200

    creates lib folder with atm.rb file, adding the same code from previous work see repo 'https://github.com/Saralundkvist86/atm.git'. Greates atm_spec.rb and also adding all code from previous work. See same repo

---
+++ lib/atm.rb
@@ -0,0 +1,41 @@
+require 'date'
+class Atm
+    attr_accessor :funds
+
+    def initialize
+        @funds = 1000
+    end
+
+    def withdraw(amount, account)
+        case
+        when insufficient_funds_in_account?(amount, account)
+            { status: false, message: 'insufficient funds in account', date: Date.today}
+        when insufficient_funds_in_atm?(amount)
+            { status: false, message: 'insufficient funds in ATM', date: Date.today}
+        when card_expired?(account.exp_date)
+            { status: false, message: 'card expired', date: Date.today }
+        else
+            perform_transaction(amount, account)
+        end
+    end
+
+    private
+
+    def insufficient_funds_in_account?(amount, account)
+        amount > account.balance
+    end
+
+    def perform_transaction(amount, account)
+        @funds -= amount
+        account.balance = account.balance - amount
+        { status: true, message: 'success', date: Date.today, amount: amount }
+    end
+
+    def insufficient_funds_in_atm?(amount)
+        @funds < amount
+    end
+
+    def card_expired?(exp_date)
+        Date.strptime(exp_date, '%m/%y') < Date.today
+    end
+end 
