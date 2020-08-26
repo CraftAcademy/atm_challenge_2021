1e14a8e12d411376cca7814f4125a1f66859a132
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-08-26 16:34:35 +0200

    Test failing when created array

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -25,8 +25,9 @@ describe Atm do
 
     it 'allow withdraw if the account has enough balance.' do
 
-        expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+        expected_output = { status: true, message: 'success', date: Date.today, amount: 45, bills: [20, 20, 5] }
         expect(subject.withdraw(45, '1234', account)).to eq expected_output
+
 
     end
 
