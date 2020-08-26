7a0bbf07fb68f6d472e82f9dc0d9ed9c38f8f1ea
SebastianN97 <sebbe132132@gmail.com>
2020-08-26 16:38:03 +0200

    creating a test 'is expected to have an expiry date on initialize. Adding STANDARD_VALIDITY_YRS on account.rb

--- lib/account.rb
+++ lib/account.rb
@@ -1,12 +1,16 @@
+require 'date'
 class Account
+    STANDARD_VALIDITY_YRS = 5
+
+
     attr_accessor :pincode
     attr_accessor :balance
     attr_accessor :account_status
     attr_accessor :exp_date
-    attr_accessor :account_owner
+    #attr_accessor :account_owner
 
     def initialize(pincode)
-        @pincode = '1234'
+        @pincode = rand(1000...9999)
     end
 
     def initialize(balance)
@@ -18,11 +22,15 @@ class Account
     end
 
     def initialize(exp_date)
-        @exp_date = '04/22'
+        @exp_date = 04/22
     end
 
-    def initialize(account_owner)
-        @account_owner = account_owner
+  #  def initialize(account_owner)
+  #      @account_owner = account_owner
+    #end
+
+    def set_expire_date
+        Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%Y')
     end
 end
 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,8 +3,13 @@ require './lib/account.rb'
 describe Account do
 
     it 'check that pincode has 4 digits' do
-    number = 1234
-    number_length = Math.log10(number).to_i + 1
-    expect(number_length).to eq 4
+        number = 1234
+        number_length = Math.log10(number).to_i + 1
+        expect(number_length).to eq 4
+    end
+
+    it 'is expected to have an expiry date on initialize' do
+        expected_date = Date.today.next_year(5).strftime("%m/%y")
+        expect(subject.exp_date(04/22)).to eq expected_date
     end
 end
