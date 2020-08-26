72033af60ce2b7a7604e0ac0d356f93ef231e00c
sara <saralundkvist86@gmail.com>
2020-08-26 16:56:13 +0200

    creating method set_expire_date, to calaculate exp date 5 years from Date.today, using method as argument for @exp_date. Test 'Account is expected to have an expiry date on initialize' now passes

--- lib/account.rb
+++ lib/account.rb
@@ -3,34 +3,21 @@ class Account
     STANDARD_VALIDITY_YRS = 5
 
 
-    attr_accessor :pincode
-    attr_accessor :balance
-    attr_accessor :account_status
-    attr_accessor :exp_date
-    #attr_accessor :account_owner
-
-    def initialize(pincode)
+    attr_accessor :pincode, :balance, :account_status, :exp_date, :account_owner
+   
+  
+    def initialize
         @pincode = rand(1000...9999)
-    end
-
-    def initialize(balance)
-        @balance = balance
-    end
-
-    def initialize(account_status)
+        @balance = 0
         @account_status = :active
+        @exp_date = set_expire_date
+        @account_owner = account_owner
     end
-
-    def initialize(exp_date)
-        @exp_date = 04/22
-    end
-
-  #  def initialize(account_owner)
-  #      @account_owner = account_owner
-    #end
+    
+private
 
     def set_expire_date
-        Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%Y')
+        Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
     end
 end
 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -10,6 +10,6 @@ describe Account do
 
     it 'is expected to have an expiry date on initialize' do
         expected_date = Date.today.next_year(5).strftime("%m/%y")
-        expect(subject.exp_date(04/22)).to eq expected_date
+        expect(subject.exp_date).to eq expected_date
     end
 end
