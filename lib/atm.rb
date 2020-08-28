e1e92454ae2c9b1a979a2804180529e2779edc16
SebastianN97 <sebbe132132@gmail.com>
2020-08-28 13:34:23 +0200

    installing rubocop

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,68 +1,66 @@
 require 'date'
-#require './lib/account.rb'
-#require './lib/person.rb'
+# require './lib/account.rb'
+# require './lib/person.rb'
 class Atm
-    attr_accessor :funds
+  attr_accessor :funds
 
-    def initialize
-        @funds = 1000
+  def initialize
+    @funds = 1000
+  end
+
+  def withdraw(amount, pincode, account)
+    if insufficient_funds_in_account?(amount, account)
+      { status: false, message: 'insufficient funds in account', date: Date.today }
+    elsif insufficient_funds_in_atm?(amount)
+      { status: false, message: 'insufficient funds in ATM', date: Date.today }
+    elsif incorrect_pin?(pincode, account.pincode)
+      { status: false, message: 'wrong pin', date: Date.today }
+    elsif card_expired?(account.exp_date)
+      { status: false, message: 'card expired', date: Date.today }
+    elsif account_disabled?(account.account_status)
+      { status: false, message: 'account status is disabled', date: Date.today }
+    else
+      perform_transaction(amount, account)
     end
+  end
 
-    def withdraw(amount, pincode, account) 
-        case
-        when insufficient_funds_in_account?(amount, account)
-            { status: false, message: 'insufficient funds in account', date: Date.today}
-        when insufficient_funds_in_atm?(amount)
-            { status: false, message: 'insufficient funds in ATM', date: Date.today}
-        when incorrect_pin?(pincode, account.pincode)
-            { status: false, message: 'wrong pin', date: Date.today }
-        when card_expired?(account.exp_date)
-            { status: false, message: 'card expired', date: Date.today }
-        when account_disabled?(account.account_status)
-            { status: false, message: 'account status is disabled', date: Date.today }
-        else
-            perform_transaction(amount, account)
-        end
+  private
+
+  def insufficient_funds_in_account?(amount, account)
+    amount > account.balance
+  end
+
+  def perform_transaction(amount, account)
+    @funds -= amount
+    account.balance = account.balance - amount
+    { status: true, message: 'success', date: Date.today, amount: amount, bills: add_bills(amount) }
+  end
+
+  def add_bills(amount)
+    denominations = [20, 10, 5]
+    bills = []
+    denominations.each do |bill|
+      while amount - bill >= 0
+        amount -= bill
+        bills << bill
+      end
     end
+    bills
+  end
 
-    private
+  def insufficient_funds_in_atm?(amount)
+    @funds < amount
+  end
 
-    def insufficient_funds_in_account?(amount, account)
-        amount > account.balance
-    end
+  def card_expired?(exp_date)
+    Date.strptime(exp_date, '%m/%y') < Date.today
+  end
 
-    def perform_transaction(amount, account)
-        @funds -= amount
-        account.balance = account.balance - amount
-        { status: true, message: 'success', date: Date.today, amount: amount, bills: add_bills(amount) }
-    end
+  def incorrect_pin?(pincode, actual_pin)
+    pincode != actual_pin
+  end
 
-    def add_bills(amount)
-        denominations = [20, 10, 5]
-        bills = []
-        denominations.each do |bill|
-            while amount - bill >= 0
-                amount -= bill
-                bills << bill
-            end
-        end
-        bills
-        
-    end
-
-    def insufficient_funds_in_atm?(amount)
-        @funds < amount
-    end
-
-    def card_expired?(exp_date)
-        Date.strptime(exp_date, '%m/%y') < Date.today
-    end
-
-    def incorrect_pin?(pincode, actual_pin)
-        pincode != actual_pin
-    end
-
-    def account_disabled?(account_status)
-        account_status != :active
-    end
+  def account_disabled?(account_status)
+    account_status != :active
+  end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -2,74 +2,68 @@ require 'date'
 require './lib/account.rb'
 require './lib/atm.rb'
 
+class Person
+  attr_accessor :name, :cash, :account
 
-class Person 
+  def initialize(attrs = {})
+    @name = set_name(attrs[:name])
+    @cash = 500                   # set_cash(attrs[:cash])
+    @account = nil # set_account(attrs[:account])
+  end
 
-    attr_accessor :name, :cash, :account
+  def create_account
+    @account = Account.new(owner: self)
+  end
 
-    def initialize(attrs = {})
-        @name = set_name(attrs[:name])
-        @cash = 500                   #set_cash(attrs[:cash])
-        @account = nil               #set_account(attrs[:account])
+  def deposit(amount)
+    @account.nil? ? missing_account : deposit_funds(amount)
+  end
 
-    end
+  def withdraw(args = {})
+    @account.nil? ? missing_account : withdraw_funds(args)
+  end
 
-    def create_account
-        @account = Account.new(owner: self)
-    end
+  private
 
-    def deposit(amount)
-        @account == nil ? missing_account : deposit_funds(amount)
-    end
+  def set_name(name)
+    name.nil? ? missing_name : name
+  end
 
-    def withdraw(args = {})
-        @account == nil ? missing_account : withdraw_funds(args)
-    end
+  # def set_cash(cash)
+  # @cash = amount
+  # end
 
+  # def set_account(account)
+  #   status == nil ? missing_account : @account = status
+  # end
 
-    private
+  def missing_name
+    raise 'A name is required'
+  end
 
-    def set_name(name)
-        name == nil ? missing_name : name
-    end
+  def deposit_funds(amount)
+    @cash -= amount
+    @account.balance += amount
+  end
 
-    #def set_cash(cash)
-       # @cash = amount
-    #end
+  def missing_account
+    raise 'No account present'
+  end
 
-    #def set_account(account)
-     #   status == nil ? missing_account : @account = status
-    #end
+  def withdraw_funds(args)
+    args[:atm].nil? ? missing_atm : atm = args[:atm]
+    account = @account
+    amount = args[:amount]
+    pin = args[:pin]
+    response = atm.withdraw(amount, pin, account)
+    response[:status] == true ? increase_cash(response) : response
+  end
 
-    def missing_name
-        raise 'A name is required'
-    end
+  def increase_cash(response)
+    @cash += response[:amount]
+  end
 
-    def deposit_funds(amount)
-        @cash -= amount
-        @account.balance += amount
-    end
-  
-    def missing_account
-        raise RuntimeError, 'No account present'
-    end
-
-    def withdraw_funds(args)
-        args[:atm] == nil ? missing_atm : atm = args[:atm]
-        account = @account
-        amount = args[:amount]
-        pin = args[:pin]
-        response = atm.withdraw(amount, pin, account)
-        response[:status] == true ? increase_cash(response) : response
-    end
-
-    def increase_cash(response)
-        @cash += response[:amount]
-    end
-
-    def missing_atm
-        raise RuntimeError, 'An ATM is required'
-    end
+  def missing_atm
+    raise 'An ATM is required'
+  end
 end
-
-
