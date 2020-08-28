c2c72bfb20ac5d0dd2c7b1029eb5222166184bbd
emiliano-ma <emilianomainero@gmail.com>
2020-08-28 23:33:01 +0200

    adding the tests: can deposit funds, funds are added to the account balance and deducted from cash, cannot deposit funds with all the implementation code to pass them

--- lib/account.rb
+++ lib/account.rb
@@ -1,32 +1,33 @@
 require 'date'
 
 class Account
-    attr_accessor :account_status, :exp_date, :pin_code, :balance, :owner
-   
+  attr_accessor :account_status, :exp_date, :pin_code, :balance, :owner
 
-    STANDARD_VALIDITY_YEARS = 5
+  STANDARD_VALIDITY_YEARS = 5
 
-    def initialize(attrs = {})
-        @account_status = :active
-        @pin_code = rand(1000..9999)
-        @exp_date = Date.today.next_year(Account::STANDARD_VALIDITY_YEARS).strftime("%m/%y")
-        @balance = 0
-        set_owner(attrs[:owner])
-    end
+  def initialize(attrs = {})
+    @account_status = :active
+    @pin_code = rand(1000..9999)
+    @exp_date = Date.today.next_year(Account::STANDARD_VALIDITY_YEARS).strftime('%m/%y')
+    @balance = 0
+    set_owner(attrs[:owner])
+  end
 
-    def deactivate
-        @account_status = :deactivated
-    end
+  def deactivate
+    @account_status = :deactivated
+  end
 
-    def length_check(pin_code)
-        number_length = Math.log10(pin_code).to_i + 1
-    end
+  def length_check(pin_code)
+    number_length = Math.log10(pin_code).to_i + 1
+  end
 
-    private
-    def set_owner(obj)
-        obj == nil ?  missing_owner : @owner = obj
-    end
-    def missing_owner
-        raise "An Account owner is required"
-    end
+  private
+
+  def set_owner(obj)
+    obj.nil? ? missing_owner : @owner = obj
+  end
+
+  def missing_owner
+    raise 'An Account owner is required'
+  end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,26 +1,40 @@
 require './lib/account'
+require 'pry'
 
 class Person
-    attr_accessor :name, :cash, :account
-    
-    def initialize(attrs = {}) 
-        @name = set_name(attrs[:name])
-        @cash = 0
-        @account = nil
-    end
+  attr_accessor :name, :cash, :account
 
+  def initialize(attrs = {})
+    @name = set_name(attrs[:name])
+    @cash = 0
+    @account = nil
+  end
 
+  def create_account
+    @account = Account.new(owner: self)
+    binding.pry
+  end
 
-    def set_name(name)
-        name == nil ? missing_name : name
-    end
+  def deposit(amount)
+    @account.nil? ? missing_account : deposit_funds(amount)
+  end
 
-    def missing_name
-        raise ArgumentError, 'A name is required'
-    end
+  private
 
-    def create_account
-        @account = Account.new(owner:self)
-    end
+  def deposit_funds(amount)
+    @cash -= amount
+    @account.balance += amount 
+  end
 
+  def missing_account
+    raise RuntimeError, 'No account present'
+  end
+
+  def set_name(name)
+    name.nil? ? missing_name : name
+  end
+
+  def missing_name
+    raise ArgumentError, 'A name is required'
+  end
 end
