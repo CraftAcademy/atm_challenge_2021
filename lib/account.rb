075f870bd1d9ca07d460408a25faea2e703ab681
Lucas Knudsen <laksteelhouse@gmail.com>
2021-01-23 16:25:58 +0100

    Working on instance double Person. Testing that account has an owner, otherwise raises error. Passing

--- lib/account.rb
+++ lib/account.rb
@@ -2,23 +2,32 @@ require 'date'
 require 'pry'
 # pin_code, :balance, :account_status, :exp_date, :owner
 class Account
-    attr_accessor :pin_code, :exp_date, :account_status
+    attr_accessor :pin_code, :exp_date, :account_status, :owner
     STANDARD_VALIDITY_YEARS = 5
-    def initialize
+    def initialize(owner_arg = {})
        @pin_code = rand(1000..9999)
        @exp_date = set_expire_date
        @account_status = :active
+       @owner = set_owner(owner_arg[:owner])
     end
     
-    def set_expire_date
-        Date.today.next_year(STANDARD_VALIDITY_YEARS).strftime('%m/%y')
-    end
-
     def deactivate
         @account_status = :deactivated
     end
+
+    private
+
+    def set_expire_date
+        Date.today.next_year(STANDARD_VALIDITY_YEARS).strftime('%m/%y')
+    end
     
-    
+    def set_owner(owner)
+        owner == nil ? raise_owner_error : owner
+    end
+
+    def raise_owner_error
+        raise 'Account doesnt have an owner'
+    end
 
 
 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,9 +2,22 @@ require './lib/account.rb'
 require 'date'
 
 describe Account do
-    it 'check the lenght of the pin code' do  
+    
+    let(:person) { instance_double('Person', name: 'Noel') }
+    subject { described_class.new({owner: person})}
+
+    it 'is expected to have a owner' do
+        expect(subject.owner).to eq person
+    end
+
+    it 'raises an error if no owner is given' do
+        expect { described_class.new }.to raise_error 'Account doesnt have an owner'
+    end
+
+    it 'check the length of the pin code' do  
         expect(subject.pin_code.to_s.length).to eq 4
     end
+
     it 'is expected to have an expiry date on initialize' do
         expected_date = Date.today.next_year(5).strftime("%m/%y")
         expect(subject.exp_date).to eq expected_date
