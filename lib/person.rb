07f07a8a9e7b49c6fbbd0e0bddd0632e60ed53e7
Lucas Knudsen <laksteelhouse@gmail.com>
2021-01-23 16:52:06 +0100

    Making Person able to create an Account that is an instance of Account and has himself as owner. Passing

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,28 @@
 require 'date'
 require 'pry'
+require './lib/account.rb'
 
 class Person 
+    attr_accessor :name, :cash, :account
 
-    
+    def initialize(name = {})
+        @name = set_name(name[:name])
+        @cash = 0
+        @account
+    end
+
+    def open_account
+        @account = Account.new({owner: @name})
+    end
+
+    private
+
+    def set_name(name)
+        name == nil ? raise_name_error : name
+    end
+
+    def raise_name_error
+        raise 'A name is required'
+    end
 
 end
--- spec/person.spec.rb
+++ spec/person.spec.rb
@@ -1,2 +1,43 @@
 require 'date'
 require './lib/person.rb'
+
+describe Person do
+
+    subject { described_class.new(name: 'Noel') }
+
+    it 'takes an argument of name and adds it as attribute on initialize' do
+        expect(subject.name).not_to be nil
+    end
+
+    it 'raises an error if no name is given' do
+        expect { described_class.new }.to raise_error 'A name is required'
+    end
+
+    it 'is expected to have a :cash attribute with the value of 0 on initialize' do
+        expect(subject.cash).to eq 0
+    end
+
+    it 'is expected to have an :account attribute with no value' do
+        expect(subject.account).to be nil
+    end
+
+    describe 'can create an Account' do
+        before {subject.open_account }
+        
+        it 'that is an instance of the Account class' do
+            expect(subject.account).to be_an_instance_of Account
+        end
+
+        it 'with himself as the owner lol' do
+            expect(subject.account.owner).to be subject.name
+        end
+
+    end
+
+    describe 'can manage funds if an account been created' do
+        
+
+        
+    end
+
+end
