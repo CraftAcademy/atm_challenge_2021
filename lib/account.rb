f2652494ac9f18ad03cca3eb0627d5724e37bfb9
August <august.enhager@gmail.com>
2021-09-02 18:13:40 +0200

    added account status test using class method

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,21 @@
 class Account
   STANDARD_VALIDITY_YRS = 5
+  attr_accessor :account_status
 
+  def initialize
+    @account_status = :active
+  end
 
   def set_expire_date
     Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
     
   end
+
+  private
+
+  def self.deactivate(account)
+   account.account_status = :deactivated 
+  end
+
+
 end
