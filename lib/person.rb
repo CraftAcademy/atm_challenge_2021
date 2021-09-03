86a351f81a4b799ae6e890fd7c5a3e7b5fddcb21
maxarvid <max.arvid.anderson@gmail.com>
2021-09-03 12:46:02 +0200

    adds account attribute to Person class

--- lib/person.rb
+++ lib/person.rb
@@ -1,24 +1,19 @@
 class Person
+  STARTING_CASH = 0
+  attr_accessor :name, :cash, :account
 
- STARTING_CASH = 0
- attr_accessor :name, :cash
-
- def initialize(attrs = {})
+  def initialize(attrs = {})
     set_name(attrs[:name])
     @cash = Person::STARTING_CASH
- end
+  end
 
- 
+  private
 
+  def set_name(obj)
+    obj.nil? ? missing_name : @name = obj
+  end
 
- private
-
- def set_name(obj)
-   obj.nil? ? missing_name : @name = obj
- end
-
- def missing_name
-   raise 'A name is required'
- end
-
+  def missing_name
+    raise 'A name is required'
+  end
 end
