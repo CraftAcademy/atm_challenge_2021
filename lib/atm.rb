1a73fd4db86e0ed0e771f36604944895c0b49323
Roberto <maisto_roberto@hotmail.it>
2022-02-01 14:57:42 +0100

    all tests passed

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,7 +1,34 @@
-class Atm 
-attr_accessor :funds
+require 'date'
 
-def initialize
+class Atm
+  attr_accessor :funds
+
+  def initialize
     @funds = 1000
-end
+  end
+
+  def withdraw(amount, account)
+    if insufficient_funds_in_account?(amount, account)
+      nil
+    else
+      perform_transaction(amount, account)
+    end
+  end
+
+  private
+
+  def insufficient_funds_in_account?(amount, account)
+    amount > account.balance
+  end
+
+  def perform_transaction(amount, account)
+    @funds -= amount
+    account.balance = account.balance - amount
+    {
+      status: true,
+      message: 'success',
+      date: Date.today,
+      amount: amount
+    }
+  end
 end
