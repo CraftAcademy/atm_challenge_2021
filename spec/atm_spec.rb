f610aac87f028bf3487a7a44b4a23ede25da8a82
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-02-01 14:57:14 +0100

    Pass all tests

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,8 +1,42 @@
-require './lib/atm.rb'
+require './lib/atm'
 
 describe Atm do
-    subject { Atm.new } # can be omitted
-    it 'is expected to hold $1000 when instantiated' do
-        expect(subject.funds).to eq 1000
-    end
+  subject { Atm.new } 
+  let(:account) { instance_double('Account') }
+  it 'is expected to hold $1000 when instantiated' do
+    expect(subject.funds).to eq 1000
+  end
+
+end
+
+describe Atm do
+  subject { Atm.new }
+  let(:account) { instance_double('Account')}
+
+  before do
+    allow(account).to receive(:balance).and_return(100)
+    allow(account).to receive(:balance=)
+  end
+
+  it 'is expected to reduce funds on withdraw' do
+    subject.withdraw 50, account
+    expect(subject.funds).to eq 950
+  end
+
+  # or using a slightly more precise matcher:
+
+  it 'is expected to reduce funds on withdraw' do
+    expect { subject.withdraw 50, account }
+      .to change { subject.funds }.from(1000).to(950)
+  end
+
+  it 'is expected to allow withdraw if account has enough balance.' do
+    expected_output = {
+      status: true,
+      message: 'success',
+      date: Date.today,
+      amount: 45
+    }
+    expect(subject.withdraw(45, account)).to eq expected_output
+  end
 end
