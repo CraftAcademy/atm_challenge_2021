4c025216e84357536bc17598e0a6ee8f177ff5e8
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-02-02 12:59:09 +0100

    passed test of rejection of wrong pin

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,7 +2,7 @@ require './lib/atm'
 
 describe Atm do
   subject { Atm.new } 
-  let(:account) { instance_double('Account', pin_code: '1234') }
+  let(:account) { instance_double('Account', pin_code: '1234', exp_date: '04/18') }
   before do
     allow(account).to receive(:balance).and_return(100)
     allow(account).to receive(:balance=)
@@ -23,12 +23,7 @@ describe Atm do
   end
 
   it 'is expected to allow withdraw if account has enough balance.' do
-    expected_output = {
-      status: true,
-      message: 'success',
-      date: Date.today,
-      amount: 45
-    }
+    expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
     expect(subject.withdraw(45, '1234', account)).to eq expected_output
   end
 
@@ -41,6 +36,17 @@ describe Atm do
     subject.funds = 50
     expected_output = { status: false, message: 'insufficient funds in ATM', date: Date.today }
     expect(subject.withdraw(100, '1234', account)).to eq expected_output
+  end
+
+  it 'reject withdraw if the pin is wrong' do
+    expected_output = { status: false, message: 'wrong pin', date: Date.today }
+    expect(subject.withdraw(50, '9999', account)). to eq expected_output
+  end
+
+  it 'reject withdraw if the card is expired' do
+    allow(account).to receive(:exp_date).and_return('12/15')
+    expected_output = { status: false, message: 'card expired', date: Date.today }
+    expect(subject.withdraw(6, '1234', account)).to eq expected_output
   end
   
 end
